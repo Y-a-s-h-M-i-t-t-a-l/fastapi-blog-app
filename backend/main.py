8afa336b6355d581f94a6ac23a1146472729f326
Yash Mittal <yashmittal052@gmail.com>
2025-08-05 19:04:59 +0530

    Second commit

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
 from core.config import settings
 
-app = FastAPI(title=settings.PROJECT_TITLE, version = settings.PROJECT_VERSION)
+
+def start_application():
+    app = FastAPI(title=settings.PROJECT_TITLE, version = settings.PROJECT_VERSION)
+    return app
+
+app = start_application()
 
 @app.get("/")
 def hello():
